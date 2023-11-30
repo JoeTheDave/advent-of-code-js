8fe25746e19203fbaf2cabfa49260d192bdb78b1
Joe Davis <jdavis@grayshift.com>
2023-11-30 05:18:36 -0600

    sun-setting old solution

---
+++ legacy/app/routes/$year/$day.tsx
@@ -0,0 +1,46 @@
+import { useLoaderData } from '@remix-run/react'
+import { useWindowSize } from '@react-hook/window-size'
+import getVisualizationComponent from 'legacy/app/lib/getVisualizationComponent'
+
+import type { LoaderFunction } from '@remix-run/server-runtime'
+import type { VisualizationComponentInfo } from 'legacy/app/lib/types/VisualizationComponentInfo'
+
+interface LoaderData {
+  solutionInfo: {
+    year: number
+    day: number
+  }
+}
+
+export const loader: LoaderFunction = async ({ params, request }) => {
+  console.log(__dirname)
+
+  const data: LoaderData = {
+    solutionInfo: {
+      year: parseInt(params.year as string),
+      day: parseInt(params.day as string),
+    },
+  }
+
+  return data
+}
+
+export default function SeedRoute() {
+  const { solutionInfo } = useLoaderData<LoaderData>()
+  const [width, height] = useWindowSize()
+
+  const Component = getVisualizationComponent(
+    solutionInfo.year,
+    solutionInfo.day,
+  )
+
+  const info: VisualizationComponentInfo = {
+    solutionInfo,
+    browserInfo: {
+      width,
+      height,
+    },
+  }
+
+  return <Component componentInfo={info} />
+}
