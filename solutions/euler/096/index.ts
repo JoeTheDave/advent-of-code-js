3acda72b5e5be5cec4bf6057dacdae21ea7eb537
Joe Davis <jdavis@grayshift.com>
2024-08-09 09:11:26 -0500

    progress on 96 - sudoku

--- solutions/euler/096/index.ts
+++ solutions/euler/096/index.ts
@@ -2,8 +2,8 @@
 // https://projecteuler.net/problem=96
 
 import { readFileSync } from 'fs'
-import _ from 'lodash'
-
+import { uniq, range, intersection } from 'lodash'
+import { getCombinations } from '@/lib/sequence'
 export const displayName = 'EULER | Problem 96 | Su Doku'
 export const complete = false
 
@@ -79,7 +79,7 @@ class Sudoku {
       cell.rowAssociations = this.cells.filter(c => c.rowId === cell.rowId && c.id !== cell.id)
       cell.colAssociations = this.cells.filter(c => c.colId === cell.colId && c.id !== cell.id)
       cell.gridAssociations = this.cells.filter(c => c.gridId === cell.gridId && c.id !== cell.id)
-      cell.allAssociations = _.uniq([...cell.rowAssociations, ...cell.colAssociations, ...cell.gridAssociations])
+      cell.allAssociations = uniq([...cell.rowAssociations, ...cell.colAssociations, ...cell.gridAssociations])
     })
   }
 
@@ -105,7 +105,7 @@ class Sudoku {
       this.cells
         .filter(cell => cell.assignment === i)
         .reduce((collective, cell) => {
-          return _.uniq([...collective, ...cell.allAssociations])
+          return uniq([...collective, ...cell.allAssociations])
         }, [] as Cell[])
         .forEach(cell => cell.marks.delete(i))
       this.cells.filter
@@ -116,10 +116,14 @@ class Sudoku {
     return this.cells.filter(cell => !cell.assignment).length
   }
 
+  marksCount = () => {
+    return this.cells.reduce((sum, cell) => sum + cell.marks.size, 0)
+  }
+
   validateSolution = () => {
     let valid = true
     this.allSectors.forEach(sector => {
-      _.range(1, 10).forEach(num => {
+      range(1, 10).forEach(num => {
         if (sector.filter(cell => cell.assignment === num).length !== 1) {
           valid = false
         }
@@ -144,11 +148,11 @@ class Sudoku {
   }
 
   strategyEliminateSkeweredMarks = () => {
-    _.range(9).forEach(gridId => {
-      _.range(1, 10).forEach(num => {
+    range(9).forEach(gridId => {
+      range(1, 10).forEach(num => {
         const gridCellsWithMark = this.grids[gridId].filter(cell => cell.marks.has(num))
         const [possibilityRows, possibilityCols] = gridCellsWithMark.reduce(
-          (collection, option) => [_.uniq([...collection[0], option.rowId]), _.uniq([...collection[1], option.colId])],
+          (collection, option) => [uniq([...collection[0], option.rowId]), uniq([...collection[1], option.colId])],
           [[], []] as number[][],
         )
         if (possibilityRows.length === 1) {
@@ -169,21 +173,158 @@ class Sudoku {
     })
   }
 
+  strategyEliminateDoubleSkeweredMarks = () => {
+    for (let a = 0; a <= 2; a++) {
+      for (let b = 0; b <= 2; b++) {
+        const gridRow = range(a * 3, a * 3 + 3)
+        const gridRowTarget = gridRow[b]
+        const gridRowSources = gridRow.filter(id => id !== gridRowTarget)
+
+        const gridCol = range(a, 9, 3)
+        const gridColTarget = gridCol[b]
+        const gridColSources = gridCol.filter(id => id !== gridColTarget)
+
+        for (let n = 1; n <= 9; n++) {
+          const rowGridOneRows = uniq(
+            this.grids[gridRowSources[0]].filter(cell => cell.marks.has(n)).map(cell => cell.rowId),
+          )
+          const rowGridTwoRows = uniq(
+            this.grids[gridRowSources[1]].filter(cell => cell.marks.has(n)).map(cell => cell.rowId),
+          )
+          if (rowGridOneRows.length === 2 && rowGridTwoRows.length === 2) {
+            const pointingRows = intersection(rowGridOneRows, rowGridTwoRows)
+            if (pointingRows.length === 2) {
+              this.grids[gridRowTarget]
+                .filter(cell => (cell.rowId === pointingRows[0] || cell.rowId === pointingRows[1]) && cell.marks.has(n))
+                .forEach(cell => cell.marks.delete(n))
+            }
+          }
+
+          const colGridOneCols = uniq(
+            this.grids[gridColSources[0]].filter(cell => cell.marks.has(n)).map(cell => cell.colId),
+          )
+          const colGridTwoCols = uniq(
+            this.grids[gridColSources[1]].filter(cell => cell.marks.has(n)).map(cell => cell.colId),
+          )
+          if (colGridOneCols.length === 2 && colGridTwoCols.length === 2) {
+            const pointingCols = intersection(colGridOneCols, colGridTwoCols)
+            if (pointingCols.length === 2) {
+              this.grids[gridColTarget]
+                .filter(cell => (cell.colId === pointingCols[0] || cell.colId === pointingCols[1]) && cell.marks.has(n))
+                .forEach(cell => cell.marks.delete(n))
+            }
+          }
+        }
+      }
+    }
+  }
+
+  strategyNakedSets = () => {
+    this.allSectors.forEach(sector => {
+      range(2, 5).forEach(setSize => {
+        const potentialMatches = sector.filter(cell => cell.marks.size === setSize)
+        const uniqSets = potentialMatches.reduce((matches, cell) => {
+          const cellMatches = potentialMatches.filter(
+            cellMatch => intersection(Array.from(cellMatch.marks), Array.from(cell.marks)).length === setSize,
+          )
+          if (cellMatches.length === setSize) {
+            matches[Array.from(cell.marks).sort().join('')] = cellMatches
+          }
+          return matches
+        }, {} as { [key: string]: Cell[] })
+        Object.keys(uniqSets).forEach(setKey => {
+          const set = uniqSets[setKey]
+          const values = setKey.split('').map(n => parseInt(n))
+          sector.forEach(cell => {
+            if (!set.includes(cell)) {
+              values.forEach(val => cell.marks.delete(val))
+            }
+          })
+        })
+      })
+    })
+  }
+
+  strategyHiddenSets = () => {
+    // Hidden pair (4,9) in col 7
+    this.allSectors.forEach(sector => {
+      range(2, 5).forEach(setSize => {
+        const numberGroupings = range(1, 10).reduce((numberGroups, num) => {
+          const cellsWithMark = sector.filter(cell => cell.marks.has(num))
+          if (cellsWithMark.length === setSize) {
+            numberGroups[`${num}`] = cellsWithMark
+          }
+          return numberGroups
+        }, {} as { [key: string]: Cell[] })
+        const groupingKeys = Object.keys(numberGroupings)
+        if (groupingKeys.length === setSize) {
+          const groupingSignatures = groupingKeys.map(key =>
+            numberGroupings[key]
+              .map(cell => cell.id)
+              .sort()
+              .join('|'),
+          )
+          if (groupingSignatures.every(signature => signature === groupingSignatures[0])) {
+            numberGroupings[groupingKeys[0]].forEach(cell => {
+              range(1, 10)
+                .filter(n => !groupingKeys.includes(`${n}`))
+                .forEach(n => cell.marks.delete(n))
+            })
+          }
+        }
+      })
+    })
+  }
+
+  strategyXWing = () => {
+    // Step 9: X-Wing in cells ( A6 , A9 , I6 , I9 )
+    // The candidate value 6 must be in one of these cells.
+    // Actions:
+    // Candidate 6 removed from cells ( G6, D9, G9 )
+
+    range(1, 10).forEach(num => {
+      const potentialRows = this.rows
+        .map(row => row.filter(cell => cell.marks.has(num)))
+        .filter(row => row.length === 2)
+
+      if (potentialRows.length >= 2) {
+        getCombinations(range(potentialRows.length), 2).forEach(potentialRowIdxs => {
+          const [row1, row2] = potentialRowIdxs
+          if (
+            potentialRows[row1][0].colId === potentialRows[row2][0].colId &&
+            potentialRows[row1][1].colId === potentialRows[row2][1].colId
+          ) {
+            const targetColIds = [potentialRows[row1][0].colId, potentialRows[row1][1].colId]
+            const sourceRowIds = [potentialRows[row1][0].rowId, potentialRows[row2][0].rowId]
+
+            targetColIds.forEach(colId => {
+              this.cols[colId].filter(cell => !sourceRowIds.includes(cell.id)).forEach(cell => cell.marks.delete(num))
+            })
+          }
+        })
+      }
+    })
+  }
+
   solve = () => {
     let emptyCount = 81
+    let markCount = 81 * 9
     this.initializeMarks()
     do {
       emptyCount = this.emptyCount()
+      markCount = this.marksCount()
       this.iterations++
       this.strategySingleMarkCells() // Solves 12
-      this.strategyLastRemainingNumberOptionInSector() // Solves 38
-      this.strategyEliminateSkeweredMarks()
-    } while (this.emptyCount() !== emptyCount)
+      this.strategyLastRemainingNumberOptionInSector() // Solves 40
+      this.strategyEliminateSkeweredMarks() // Solves 43
+      this.strategyEliminateDoubleSkeweredMarks() // Solves 44
+      this.strategyNakedSets() // Solves 48
+      this.strategyHiddenSets() // Solves 48
+    } while (this.emptyCount() !== emptyCount || this.marksCount() !== markCount)
+    // this.strategyXWing()
   }
 
-  draw = () => {
-    // const showMarks = [4]
-    const showMarks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  draw = (showMarks: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]) => {
     console.log('\x1b[2J\x1b[0;0H') // Clear the console
     console.log('╔═══════╤═══════╤═══════╦═══════╤═══════╤═══════╦═══════╤═══════╤═══════╗')
     console.log('║       │       │       ║       │       │       ║       │       │       ║')
@@ -228,7 +369,8 @@ class Sudoku {
       const y = 4 + cell.rowId * 4
       const markColor = '34'
       if (cell.assignment) {
-        console.log(`\x1b[${y};${x}H\x1b[${cell.fixed ? '31' : '32'}m`, `${cell.assignment}`)
+        console.log(`\x1b[${y};${x}H\x1b[47;1m\x1b[${cell.fixed ? '31' : '32'}m`, `${cell.assignment}`)
+        console.log(`\x1b[0m`)
       } else {
         cell.marks.forEach(mark => {
           if (mark === 1 && showMarks.includes(1)) {
@@ -265,18 +407,6 @@ class Sudoku {
     console.log()
     console.log()
     console.log(`Empty Count: ${this.emptyCount()}, Iterations: ${this.iterations}`)
-
-    // console.log('\x1b[2J\x1b[0;0H') // Clear the console
-
-    // // Write text at specific coordinates
-    // console.log('\x1b[10;10H', 'Text at (10, 10)')
-    // console.log('\x1b[5;20H', 'Text at (5, 20)')
-
-    // // Write colored text at specific coordinates
-    // console.log('\x1b[15;5H\x1b[31m', 'Red text at (15, 5)')
-    // console.log('\x1b[15;10H\x1b[32m', 'Green text at (20, 10)')
-    // console.log('\x1b[25;15H\x1b[33m', 'Yellow text at (25, 15)')
-    // console.log('\x1b[0m') // Reset color
   }
 }
 
@@ -289,8 +419,7 @@ const solvePuzzles = (puzzles: string[]) =>
       idx,
       solved: unsolvedCellsCount === 0,
       unsolvedCellsCount,
-      // iterations: sudoku.iterations,
-      invalid: unsolvedCellsCount === 0 && sudoku.validateSolution(),
+      invalid: unsolvedCellsCount === 0 && !sudoku.validateSolution(),
     }
   })
 
@@ -298,13 +427,21 @@ export const solution = () => {
   const rawData = readFileSync(`${__dirname}/sudoku.txt`, 'utf8')
   const sudokuPuzzles = processRawData(rawData)
 
-  const results = solvePuzzles(sudokuPuzzles)
-  console.log(results)
-  console.log(`Total Solved: ${results.filter(p => p.solved).length}`)
-
-  // const sudoku = new Sudoku(sudokuPuzzles[5])
-  // sudoku.solve()
-  // sudoku.draw()
+  const solveAll = false
+  const targetPuzzle = 6
+  const draw = true
+  if (solveAll) {
+    const results = solvePuzzles(sudokuPuzzles)
+    console.log(results.filter(p => p.solved))
+    console.log(results.filter(p => !p.solved))
+    console.log(`Total Solved: ${results.filter(p => p.solved).length}`)
+  } else {
+    const sudoku = new Sudoku(sudokuPuzzles[targetPuzzle])
+    sudoku.solve()
+    if (draw) {
+      sudoku.draw()
+    }
+  }
 
   return null
 }
